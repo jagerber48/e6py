e20f64ed512d9f8131709c1c9d3c6881568cde31
jagerber48 <justin.gerber48@gmail.com>
2020-05-28 01:43:29 -0700

    removed old Atom class

--- E6OptTrap.py
+++ E6OptTrap.py
@@ -9,102 +9,6 @@ c = const.c
 ep0 = const.epsilon_0
 
 
-class Atom:
-    """Data container for atom"""
-    def __init__(self, element):
-        if element == 'Rb87':
-            self.isboson = True
-
-            self.gammaD1 = 1 / 27.679e-9
-            self.transitionfrequencyD1 = 377.107463380e12  # Center transition frequency D1 line (Steck)
-            self.IsatD1 = 4.4876 * 1e-3 * (1e2 ** 2)  # Far-Detuned, pi-polarized, W/m^2
-            self.deffD1 = np.sqrt((c * ep0 * hbar**2 * self.gammaD1**2) / (4 * self.IsatD1))
-
-            self.gammaD2 = 1 / 26.2348e-9
-            self.transitionfrequencyD2 = 384.2304844685e12  # Center transition frequency of D2 line (Steck)
-            self.IsatD2 = 2.50399 * 1e-3 * (1e2 ** 2)  # Far-Detuned, pi-polarized, W/m^2
-            self.deffD2 = np.sqrt((c * ep0 * hbar**2 * self.gammaD2**2) / (4 * self.IsatD2))
-
-            self.gamma = 0.5 * (self.gammaD1 + self.gammaD2)  # Decay from 5P state (Steck)
-            self.transitionfrequency = 0.5 * (
-                    self.transitionfrequencyD1 + self.transitionfrequencyD2)  # Arbitrary! Check
-            self.finestructure_splitting = self.transitionfrequencyD2 - self.transitionfrequencyD1
-            self.nuclearspin = 1.5
-            self.spin = 0.5
-            self.gj = 2.00233113  # Value from Steck
-            self.mass = 1.443160648e-25  # Value from Steck
-            self.scattering_length = 100.4 * const.value('Bohr radius')
-        else:
-            print('Element not defined. Using Rubidium-87')
-            self.isboson = True
-
-            self.gammaD1 = 1 / 27.679e-9
-            self.transitionfrequencyD1 = 377.107463380e12  # Center transition frequency D1 line (Steck)
-            self.IsatD1 = 4.4876 * 1e-3 * (1e2 ** 2)  # Far-Detuned, pi-polarized, W/m^2
-            self.deffD1 = np.sqrt((c * ep0 * hbar ** 2 * self.gammaD1 ** 2) / (4 * self.IsatD1))
-
-            self.gammaD2 = 1 / 26.2348e-9
-            self.transitionfrequencyD2 = 384.2304844685e12  # Center transition frequency of D2 line (Steck)
-            self.IsatD2 = 2.50399 * 1e-3 * (1e2 ** 2)  # Far-Detuned, pi-polarized, W/m^2
-            self.deffD2 = np.sqrt((c * ep0 * hbar ** 2 * self.gammaD2 ** 2) / (4 * self.IsatD1))
-
-            self.gamma = 0.5 * (self.gammaD1 + self.gammaD2)  # Decay from 5P state (Steck)
-            self.transitionfrequency = 0.5 * (
-                    self.transitionfrequencyD1 + self.transitionfrequencyD2)  # Arbitrary! Check
-            self.finestructure_splitting = self.transitionfrequencyD2 - self.transitionfrequencyD1
-            self.nuclearspin = 1.5
-            self.spin = 0.5
-            self.gj = 2.00233113  # Value from Steck
-            self.mass = 1.443160648e-25  # Value from Steck
-            self.scattering_length = 100.4 * const.value('Bohr radius')
-
-    def rabi_freq(self, e_field, transition_dipole=None):
-        if transition_dipole is None:
-            transition_dipole = self.deffD2
-        rabi_freq = -e_field*transition_dipole/hbar
-        return rabi_freq
-
-    def optical_potential_from_intensity(self, intensity, wavelength=None, f_field=None):
-        # Convert an optical intensity into an optical potential
-        if f_field is None:
-            try:
-                f_field = const.c / wavelength
-            except TypeError:
-                print('Must provide wavelength if frequency is not specified')
-        f0_D1 = self.transitionfrequencyD1
-        f0_D2 = self.transitionfrequencyD2
-        U_rotating = (const.hbar * intensity / 8) * \
-                     (
-                             (self.gammaD1 ** 2 / (2 * np.pi * (f_field - f0_D1) * self.IsatD1))
-                             + (self.gammaD2 ** 2 / (2 * np.pi * (f_field - f0_D2) * self.IsatD2))
-                     )
-        U_counterrotating = (const.hbar * intensity / 8) * \
-                            (
-                                    (self.gammaD1 ** 2 / (2 * np.pi * (f_field + f0_D1) * self.IsatD1))
-                                    + (self.gammaD2 ** 2 / (2 * np.pi * (f_field + f0_D2) * self.IsatD2))
-                            )
-        return U_rotating + U_counterrotating
-
-    def optical_potential(self, e_field, wavelength=None, f_field=None):
-        # Convert an electric field into an optical potential
-        if f_field is None:
-            try:
-                f_field = const.c / wavelength
-            except TypeError:
-                print('Must provide wavelength if frequency is not specified')
-        rabi_D1 = self.rabi_freq(e_field, transition_dipole=self.deffD1)
-        rabi_D2 = self.rabi_freq(e_field, transition_dipole=self.deffD2)
-        f0_D1 = self.transitionfrequencyD1
-        f0_D2 = self.transitionfrequencyD2
-        detuning_D1 = 2 * np.pi * (f_field - f0_D1)
-        detuning_D2 = 2 * np.pi * (f_field - f0_D2)
-        counter_detuning_D1 = 2 * np.pi * (f_field + f0_D1)
-        counter_detuning_D2 = 2 * np.pi * (f_field + f0_D2)
-        U_rotating = (hbar / 4) * (rabi_D1**2 / detuning_D1 + rabi_D2**2 / detuning_D2)
-        U_counterrotating = (hbar / 4) * (rabi_D1**2 / counter_detuning_D1 + rabi_D2**2 / counter_detuning_D2)
-        return U_rotating + U_counterrotating
-
-
 class Beam:
     # Class to model an optical gaussian beam
     def __init__(self, waist_x, power, wavelength, waist_y=None, z0_x=0, z0_y=0):
@@ -301,7 +205,7 @@ class OptTrap:
         return
 
 
-def make_grav_pot(mass=Atom('Rb87').mass, grav_vec=(0, 0, -1),
+def make_grav_pot(mass=Rb87_Atom.mass, grav_vec=(0, 0, -1),
                   x0=(-1,)*3, xf=(1,)*3, n_steps=(10,)*3):
     def grav_func(x, y, z):
         grav_comp = np.dot([x, y, z], grav_vec)
